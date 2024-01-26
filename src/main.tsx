8d9746103b37fcc822195bda51fa3dd00980466a
Alexis Comte <alexis.comte73@icloud.com>
2024-01-26 17:40:54 +0100

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import { createRouter } from './router'
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={createRouter()} />
+  </React.StrictMode>
+)
