b3c51336e350441db1a99b0ae028c90b779e8b68
Alexis Comte <alexis.comte73@icloud.com>
2024-01-28 11:14:38 +0100

    adding search bar

---
+++ src/components/common/SearchBar/Searchbar.tsx
@@ -0,0 +1,66 @@
+import { ArrowDown, SearchIcon } from '@assets/icons'
+import { useState } from 'react'
+
+const SEARCH_OPTIONS = [
+  { id: 1, name: 'Articles', searchText: 'Rechercher des articles' },
+  { id: 2, name: 'Membres', searchText: 'Rechercher des membres' },
+  { id: 3, name: "Centre d'aide", searchText: 'Rechercher par mot-clé' }
+]
+
+export const Searchbar = () => {
+  const [selected, setSelected] = useState(SEARCH_OPTIONS[0])
+
+  const handleSelect = (id: number) => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    setSelected(SEARCH_OPTIONS.find(option => option.id === id)!)
+  }
+
+  return <div className="h-9 bg-[#f2f2f2] w-full rounded-[4px] flex">
+    <SearchSelect currentSelect={selected} onSelect={handleSelect} />
+    <div className='flex gap-x-2 items-center px-3 w-full'>
+      <SearchIcon className='text-[#999999] text-[1.1em]' />
+      <input className='bg-transparent outline-none w-full' type='text' placeholder={selected.searchText} />
+    </div>
+  </div>
+}
+
+const SearchSelect = ({ currentSelect, onSelect }: { currentSelect: { id: number, name: string, searchText: string }, onSelect: (id: number) => void }) => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleToggleMenu = () => {
+    setMenuOpen(prev => !prev)
+  }
+
+  const handleSelect = (id: number) => {
+    onSelect(id)
+  }
+
+  return (
+  <div className='select items-center flex border-[#e1e1e1] border-r-[1px] relative'>
+    <button onClick={handleToggleMenu} className="flex px-3 rounded-[4px] gap-x-1 items-center hover:bg-[#efefef] text-[#757575] h-full flex-1">
+      <span className='text-[14px] text-nowrap'>{currentSelect.name}</span>
+      <ArrowDown className='text-[1rem]' />
+    </button>
+    {menuOpen && (
+      <ul className='absolute top-11 bg-white w-[148px] rounded-[4px] border-[1px] border-[#efefef] flex flex-col text-[#4D4D4D]'>
+        {SEARCH_OPTIONS.map((option) => (<SearchOption closeMenu={() => { setMenuOpen(false) }} key={option.id} onClick={() => { handleSelect(option.id) }} name={option.name} />))}
+      </ul>
+    )}
+
+  </div>
+  )
+}
+
+const SearchOption = ({ name, onClick, closeMenu }: { name: string, onClick: () => void, closeMenu: () => void }) => {
+  const handleChangeSelection = () => {
+    onClick()
+    closeMenu()
+  }
+  return (
+    <li>
+      <button onClick={handleChangeSelection} className='hover:bg-[#fafafa] p-2 w-full text-left'>
+        {name}
+      </button>
+    </li>
+  )
+}
--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -1,7 +1,16 @@
-import { VintedIcon } from '@assets/vinted'
+import { VintedIcon } from '@assets/icons'
+import Searchbar from '@components/common/SearchBar'
 
 export const Header = () => {
-  return <header className="w-full h-[52px] py-1 flex border-b-[1px] border-[#ededed] items-center">
-    <VintedIcon className='text-[44px]' />
-  </header>
+  return <>
+    <header className="w-full h-[52px] py-1 flex border-b-[1px] border-[#ededed] items-center">
+      <VintedIcon className='text-[44px]' />
+      <div className='hidden lg:block'>
+        <Searchbar />
+      </div>
+    </header>
+    <div className='lg:hidden p-2'>
+      <Searchbar />
+    </div>
+  </>
 }
